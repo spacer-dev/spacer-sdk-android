13f92d30935888b9e7cd337113c6f2fd7a4ef89f
s-arai1 <s.arai@a-sw.jp>
2023-02-24 17:25:36 +0900

    update: change the value of a scanMills

--- sdk/src/main/java/com/spacer/sdk/data/SPRConst.kt
+++ sdk/src/main/java/com/spacer/sdk/data/SPRConst.kt
@@ -3,7 +3,7 @@ package com.spacer.sdk.data
 class SPRConst {
     companion object {
         const val BaseURL = "https://ex-app.spacer.co.jp"
-        const val ScanMills: Long = 5000
-        const val MaxRetryNum: Int = 3
+        const val ScanMills: Long = 3000
+        const val MaxRetryNum: Int = 3 
     }
 }
