6d608b04433392ef9524285b068d7b9771db68bb
s-arai1 <s.arai@a-sw.jp>
2023-04-12 12:35:24 +0900

    add: cloesedWait

--- sdk/src/main/java/com/spacer/sdk/data/api/resData/sprLocker/SPRLockerResData.kt
+++ sdk/src/main/java/com/spacer/sdk/data/api/resData/sprLocker/SPRLockerResData.kt
@@ -5,5 +5,6 @@ import java.io.Serializable
 data class SPRLockerResData(
     val id: String,
     val status: String,
-    val size: String?
+    val size: String?,
+    val closedWait: String?
 ) : Serializable
--- sdk/src/main/java/com/spacer/sdk/models/sprLocker/SPRLockerModel.kt
+++ sdk/src/main/java/com/spacer/sdk/models/sprLocker/SPRLockerModel.kt
@@ -6,13 +6,13 @@ import com.spacer.sdk.data.api.resData.sprLocker.SPRLockerResData
 import com.spacer.sdk.data.extensions.EnumExtensions.safeValueOf
 import com.spacer.sdk.values.sprLocker.SPRLockerStatus
 
-class SPRLockerModel(val id: String, val status: SPRLockerStatus, val size: String?) {
-    override fun toString() = "id:${id},status:${status.text},size:${size}"
+class SPRLockerModel(val id: String, val status: SPRLockerStatus, val size: String?, val closedWait: String?) {
+    override fun toString() = "id:${id},status:${status.text},size:${size},closedWait:${closedWait}"
 }
 
 fun SPRLockerResData.toModel(): SPRLockerModel {
     val status = safeValueOf(status, SPRLockerStatus.Unknown)
-    return SPRLockerModel(id, status, size)
+    return SPRLockerModel(id, status, size, closedWait)
 }
 
 class SPRLockerListMapper : IMapper<SPRLockerGetResData, List<SPRLockerModel>> {
